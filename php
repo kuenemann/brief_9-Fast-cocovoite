76ccf4c5b5363ec3e92c6a04aa628eb1c3305e61
kuenemann <leokuenemann2@gmail.com>
2023-05-26 15:45:36 +0200

    update

---
+++ php
@@ -0,0 +1,8 @@
+
+ Choose a name for your controller class (e.g. TinyPopsicleController):
+ > 
+
+ [ERROR] "App\Controller>OffersController" is not valid as a PHP class name (it 
+         must start with a letter or underscore, followed by any number of      
+         letters, numbers, or underscores)                                      
+
---
+++ src/Controller/OffersController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OffersController extends AbstractController
+{
+    #[Route('/offers', name: 'offers_page')]
+    public function offers(): Response
+    {
+        $pagetitle='offers';
+        return $this->render('offers/index.html.twig', [
+            'controller_name' => 'OffersController',
+            'page_title' => $pagetitle, 
+
+        ]);
+    }
+
+
+    #[Route('/offers/detail', name: 'offers_detail')]
+    public function detail(): Response
+    {
+        $pagetitle='offers/detail';
+        return $this->render('offers/detail.html.twig', [
+            'controller_name' => 'OffersController',
+            'page_title' => $pagetitle,
+        ]);
+    }
+
+
+
+    #[Route('/offers/test', name: 'offers_test')]
+    public function test(): Response
+    {
+        $pagetitle='offerts/test';
+        return $this->render('offers/test.html.twig', [
+            'controller_name' => 'OffersController',
+            'page_title' => $pagetitle,
+        ]);
+    }
+
+}
